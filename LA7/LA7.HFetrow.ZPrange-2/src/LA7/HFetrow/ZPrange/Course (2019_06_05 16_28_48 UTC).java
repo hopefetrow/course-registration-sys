b545d5ff01cf44006f504c2ddd469ad888e63080
Hope <43889347+hopefetrow@users.noreply.github.com>
2022-06-24 20:57:56 -0400

    Re-adding... again
    Left some files out

---
+++ LA7/LA7.HFetrow.ZPrange-2/src/LA7/HFetrow/ZPrange/Course (2019_06_05 16_28_48 UTC).java
@@ -0,0 +1,23 @@
+package LA7.HFetrow.ZPrange;
+
+public class Course implements ICourse {
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addStudent(String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printClassList() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ LA7/LA7.HFetrow.ZPrange-2/src/LA7/HFetrow/ZPrange/Node (2019_06_05 16_28_48 UTC).java
@@ -0,0 +1,11 @@
+package LA7.HFetrow.ZPrange;
+
+public class Node implements INode {
+
+	@Override
+	public E getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
